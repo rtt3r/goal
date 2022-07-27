758be7137180c831b5a0c1d283a97b252070d636
Anderson Ritter de Souza <anderdsouza@gmail.com>
2022-07-27 08:03:41 -0300

    ajustes command result

--- src/Seedwork/Domain/Commands/CommandResult.cs
+++ src/Seedwork/Domain/Commands/CommandResult.cs
@@ -6,27 +6,33 @@ namespace Goal.Seedwork.Domain.Commands
 {
     public class CommandResult : ICommandResult
     {
+        protected CommandResult(bool isSucceeded, ICollection<INotification> notifications)
+        {
+            IsSucceeded = isSucceeded;
+            Notifications = notifications ?? new List<INotification>();
+        }
+
         public bool IsSucceeded { get; private set; }
         public bool HasInformation => HasNotification(NotificationType.Information);
         public bool HasInputValidation => HasNotification(NotificationType.InputValidation);
         public bool HasDomainViolation => HasNotification(NotificationType.DomainViolation);
         public bool HasInternalError => HasNotification(NotificationType.InternalError);
         public bool HasExternalError => HasNotification(NotificationType.ExternalError);
-        public ICollection<INotification> Notifications { get; } = new List<INotification>();
+        public ICollection<INotification> Notifications { get; private set; } = new List<INotification>();
 
         private bool HasNotification(NotificationType type)
             => Notifications.Any(n => n.Type == type);
 
-        public static ICommandResult Success()
-            => new CommandResult { IsSucceeded = true };
+        public static ICommandResult Success(params INotification[] notifications)
+            => new CommandResult(true, notifications);
 
-        public static ICommandResult<T> Success<T>(T result)
-            => new CommandResult<T> { IsSucceeded = true, Data = result };
+        public static ICommandResult<T> Success<T>(T result, params INotification[] notifications)
+            => new CommandResult<T>(true, result, notifications);
 
-        public static ICommandResult Fail()
-            => new CommandResult { IsSucceeded = false };
+        public static ICommandResult Fail(params INotification[] notifications)
+            => new CommandResult(false, notifications);
 
-        public static ICommandResult<T> Fail<T>(T result)
-            => new CommandResult<T> { IsSucceeded = false, Data = result };
+        public static ICommandResult<T> Fail<T>(T result, params INotification[] notifications)
+            => new CommandResult<T>(false, result, notifications);
     }
 }
--- src/Seedwork/Domain/Commands/CommandResult`.cs
+++ src/Seedwork/Domain/Commands/CommandResult`.cs
@@ -1,7 +1,16 @@
+using System.Collections.Generic;
+using Goal.Seedwork.Infra.Crosscutting.Notifications;
+
 namespace Goal.Seedwork.Domain.Commands
 {
     public class CommandResult<T> : CommandResult, ICommandResult<T>
     {
+        public CommandResult(bool isSucceeded, T data, ICollection<INotification> notifications)
+            : base(isSucceeded, notifications)
+        {
+            Data = data;
+        }
+
         public T Data { get; set; }
     }
 }
