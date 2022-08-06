09d705e2e6aad650aaffc8aae957853f839e062b
Anderson Ritter de Souza <anderdsouza@gmail.com>
2022-08-06 17:34:13 -0300

    ajustes command result

--- src/Seedwork/Application/Commands/CommandResult.cs
+++ src/Seedwork/Application/Commands/CommandResult.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Goal.Seedwork.Infra.Crosscutting.Notifications;
+
 namespace Goal.Seedwork.Application.Commands
 {
     public class CommandResult : ICommandResult
@@ -7,17 +12,82 @@ namespace Goal.Seedwork.Application.Commands
         }
 
         public bool IsSucceeded { get; private set; }
+        public IEnumerable<Notification> Notifications { get; private set; } = new List<Notification>();
 
-        public static ICommandResult Success()
-            => new CommandResult { IsSucceeded = true };
+        public static ICommandResult Success(params Notification[] notifications)
+        {
+            if (notifications.Any(p => p.Type != NotificationType.Information))
+            {
+                throw new InvalidOperationException("For 'Success' result only notifications of type 'Information' are accepted.");
+            }
 
-        public static ICommandResult<TData> Success<TData>(TData data)
-            => new CommandResult<TData> { IsSucceeded = true, Data = data };
+            return new CommandResult
+            {
+                IsSucceeded = true,
+                Notifications = notifications
+            };
+        }
 
-        public static ICommandResult Fail()
-            => new CommandResult { IsSucceeded = false };
+        public static ICommandResult<TData> Success<TData>(TData data, params Notification[] notifications)
+        {
+            if (notifications.Any(p => p.Type != NotificationType.Information))
+            {
+                throw new InvalidOperationException("For 'Success' result only notifications of type 'Information' are accepted.");
+            }
 
-        public static ICommandResult<TData> Fail<TData>(TData data)
-            => new CommandResult<TData> { IsSucceeded = false, Data = data };
+            return new CommandResult<TData>
+            {
+                IsSucceeded = true,
+                Data = data,
+                Notifications = notifications
+            };
+        }
+
+        public static ICommandResult Failure(IEnumerable<Notification> notifications)
+        {
+            if (!notifications.Any(n => n.Type != NotificationType.Information))
+            {
+                throw new InvalidOperationException("For 'Failure' result it's necessary to report failure notifications.");
+            }
+
+            return new CommandResult
+            {
+                IsSucceeded = false,
+                Notifications = notifications
+            };
+        }
+
+        public static ICommandResult<TData> Failure<TData>(TData data, IEnumerable<Notification> notifications)
+        {
+            if (!notifications.Any(n => n.Type != NotificationType.Information))
+            {
+                throw new InvalidOperationException("For 'Failure' result it's necessary to report failure notifications.");
+            }
+
+            return new CommandResult<TData>
+            {
+                IsSucceeded = false,
+                Data = data,
+                Notifications = notifications
+            };
+        }
+
+        public bool HasDomainViolation()
+            => HasNotificationsOf(NotificationType.DomainViolation);
+
+        public bool HasExternalError()
+            => HasNotificationsOf(NotificationType.ExternalError);
+
+        public bool HasInternalError()
+            => HasNotificationsOf(NotificationType.InternalError);
+
+        public bool HasInformation()
+            => HasNotificationsOf(NotificationType.Information);
+
+        public bool HasInputValidation()
+            => HasNotificationsOf(NotificationType.InputValidation);
+
+        private bool HasNotificationsOf(NotificationType type)
+            => Notifications.Any(p => p.Type == type);
     }
 }
--- src/Seedwork/Application/Commands/ICommandResult.cs
+++ src/Seedwork/Application/Commands/ICommandResult.cs
@@ -1,7 +1,16 @@
+using System.Collections.Generic;
+using Goal.Seedwork.Infra.Crosscutting.Notifications;
+
 namespace Goal.Seedwork.Application.Commands
 {
     public interface ICommandResult
     {
         bool IsSucceeded { get; }
+        IEnumerable<Notification> Notifications { get; }
+        bool HasDomainViolation();
+        bool HasExternalError();
+        bool HasInternalError();
+        bool HasInformation();
+        bool HasInputValidation();
     }
 }
