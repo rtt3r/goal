151471642d3fbe55e48163e66adc64abc7d7cff9
Anderson Ritter de Souza <anderdsouza@gmail.com>
2023-03-18 00:40:26 -0300

    Coverage Event

---
+++ tests/Domain.Tests/Event/EventTests.cs
@@ -0,0 +1,57 @@
+using System;
+using FluentAssertions;
+using Xunit;
+
+
+namespace Goal.Seedwork.Domain.Tests.Event
+{
+    public class EventTests
+    {
+        private class SampleEvent : Domain.Events.Event
+        {
+            public SampleEvent()
+            {
+            }
+
+            public SampleEvent(string aggregateId)
+            {
+                AggregateId = aggregateId;
+            }
+
+            public string Data { get; set; }
+        }
+
+        [Fact]
+        public void Event_Should_Set_Default_Timestamp()
+        {
+            // Arrange & Act
+            var @event = new SampleEvent();
+
+            // Assert
+            @event.Timestamp.Should().BeCloseTo(DateTimeOffset.UtcNow, TimeSpan.FromSeconds(1));
+        }
+
+        [Fact]
+        public void Event_Should_Set_EventType()
+        {
+            // Arrange & Act
+            var @event = new SampleEvent();
+
+            // Assert
+            @event.EventType.Should().Be(nameof(SampleEvent));
+        }
+
+        [Fact]
+        public void Event_Should_Set_AggregateId_Property()
+        {
+            // Arrange
+            var aggregateId = Guid.NewGuid().ToString();
+
+            // Act
+            var @event = new SampleEvent(aggregateId);
+
+            // Assert
+            @event.AggregateId.Should().Be(aggregateId);
+        }
+    }
+}
