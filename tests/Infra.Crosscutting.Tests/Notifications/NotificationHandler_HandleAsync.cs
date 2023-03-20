c7ab9b74bdd6b365ac9b419fae053db7586d643b
Anderson Ritter de Souza <anderdsouza@gmail.com>
2023-03-19 23:58:05 -0300

    notification handler tests

---
+++ tests/Infra.Crosscutting.Tests/Notifications/NotificationHandler_HandleAsync.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Goal.Seedwork.Infra.Crosscutting.Notifications;
+using Xunit;
+
+namespace Goal.Seedwork.Infra.Crosscutting.Tests.Notifications
+{
+    public class NotificationHandler_HandleAsync
+    {
+        [Fact]
+        public async Task AddsNotification()
+        {
+            // Arrange
+            var handler = new TestNotificationHandler();
+            var notification = Notification.Information("test", "Test message");
+
+            // Act
+            await handler.HandleAsync(notification);
+
+            // Assert
+            handler.GetNotifications().Should().HaveCount(1);
+        }
+
+        private class TestNotificationHandler : NotificationHandler<Notification>
+        {
+            public override IEnumerable<Notification> GetNotifications()
+            => base.GetNotifications();
+        }
+    }
+}
