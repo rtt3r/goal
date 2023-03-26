0edc4e83e5c73b0d5f271f17147aa80f3b11ccca
Anderson Ritter de Souza <anderdsouza@gmail.com>
2023-03-26 14:46:08 -0300

    improve unit tests

--- tests/Infra.Data.Tests/Repositories/Repository_Dispose.cs
+++ tests/Infra.Data.Tests/Repositories/Repository_Dispose.cs
@@ -1,9 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace Goal.Seedwork.Infra.Data.Tests.Repositories
@@ -11,7 +9,7 @@ namespace Goal.Seedwork.Infra.Data.Tests.Repositories
     public class Repository_Dispose
     {
         [Fact]
-        public void CallsProtectedDisposeMethodWithTrue()
+        public void DisposeSimpleRepository_GivenTrue()
         {
             // Arrange
             var repository = new TestRepository();
@@ -23,14 +21,68 @@ namespace Goal.Seedwork.Infra.Data.Tests.Repositories
             repository.IsDisposed.Should().BeTrue();
         }
 
+        [Fact]
+        public void DisposeSingularityRepository_GivenTrue()
+        {
+            // Arrange
+            UniverseContext ctx = CreateContext();
+            var repository = new SingularityRepository(ctx);
+
+            // Act
+            repository.Dispose();
+
+            // Assert
+            repository.Context.Should().BeNull();
+        }
+
+        public UniverseContext CreateContext()
+            => new(CreateOptions());
+
+        private DbContextOptions CreateOptions()
+        {
+            return new DbContextOptionsBuilder()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+        }
+
+        public class UniverseContext : DbContext
+        {
+            public UniverseContext(DbContextOptions options)
+                : base(options)
+            {
+            }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder
+                    .Entity<Singularity>()
+                    .HasData(
+                        new Singularity { Id = 77, Type = "Black Hole" },
+                        new Singularity { Id = 88, Type = "Bing Bang" });
+            }
+        }
+
+        public class Singularity
+        {
+            [DatabaseGenerated(DatabaseGeneratedOption.None)]
+            public int Id { get; set; }
+
+            public string Type { get; set; }
+        }
+
         private class TestRepository : Repository
         {
             public bool IsDisposed { get; private set; }
 
             protected override void Dispose(bool disposing)
+                => IsDisposed = true;
+        }
+
+        private class SingularityRepository : Repository<Singularity>
+        {
+            public SingularityRepository(DbContext context)
+                : base(context)
             {
-                // Do actual disposal here
-                IsDisposed = true;
             }
         }
     }
