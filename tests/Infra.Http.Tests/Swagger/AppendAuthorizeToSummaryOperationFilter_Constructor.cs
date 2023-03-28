d73b9c9b995d14fe680503294d64b141878e9235
Anderson Ritter de Souza <anderdsouza@gmail.com>
2023-03-27 21:23:42 -0300

    improve unit tests

--- tests/Infra.Http.Tests/Swagger/AppendAuthorizeToSummaryOperationFilter_Constructor.cs
+++ tests/Infra.Http.Tests/Swagger/AppendAuthorizeToSummaryOperationFilter_Constructor.cs
@@ -24,7 +24,7 @@ namespace Goal.Seedwork.Infra.Http.Tests.Swagger
                 new ApiDescription(),
                 new SchemaGenerator(new SchemaGeneratorOptions(), null),
                 null,
-                typeof(MockController).GetMethod(nameof(MockController.Get)));
+                typeof(AuthorizedController).GetMethod(nameof(AuthorizedController.Get)));
 
             //Act
             filter.Apply(operation, context);
@@ -35,10 +35,40 @@ namespace Goal.Seedwork.Infra.Http.Tests.Swagger
             operation.Summary.Should().Contain("(Auth policies: policy1, policy2; roles: role1, role2)");
         }
 
-        public class MockController : ControllerBase
+        [Fact]
+        public void ApplyAnonymousInformationToOperation()
+        {
+            //Arrange 
+            var filter = new AppendAuthorizeToSummaryOperationFilter();
+            var operation = new OpenApiOperation();
+            var context = new OperationFilterContext(
+                new ApiDescription(),
+                new SchemaGenerator(new SchemaGeneratorOptions(), null),
+                null,
+                typeof(AnonymousController).GetMethod(nameof(AnonymousController.Get)));
+
+            //Act
+            filter.Apply(operation, context);
+
+            //Assert
+            operation.Responses.Should().HaveCount(0);
+            operation.Summary.Should().BeNullOrWhiteSpace();
+        }
+
+        public class AuthorizedController : ControllerBase
         {
             [HttpGet]
             [Authorize(Policy = "policy1, policy2", Roles = "role1, role2")]
+            public IActionResult Get()
+            {
+                return Ok();
+            }
+        }
+
+        public class AnonymousController : ControllerBase
+        {
+            [HttpGet]
+            [AllowAnonymous]
             public IActionResult Get()
             {
                 return Ok();
