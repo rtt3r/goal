e9dc013bfcf6786a58639d982402efe53126b77c
Anderson Ritter de Souza <anderdsouza@gmail.com>
2023-07-26 15:12:47 -0300

    improve DesignTimeDbContextFactory implementation

--- src/Seedwork/Infra.Data/DesignTimeDbContextFactory.cs
+++ src/Seedwork/Infra.Data/DesignTimeDbContextFactory.cs
@@ -14,26 +14,22 @@ public abstract class DesignTimeDbContextFactory<TContext> : IDesignTimeDbContex
     protected virtual string ConnectionStringName
         => DefaultConnectionStringName;
 
+    public IConfiguration Configuration { get; private set; }
+
     public TContext CreateDbContext(string[] args)
         => CreateDbContext(Directory.GetCurrentDirectory(), Environment.GetEnvironmentVariable(AspNetCoreEnvironment));
 
-    protected abstract TContext CreateNewInstance(DbContextOptionsBuilder<TContext> optionsBuilder, string connectionString);
+    protected abstract TContext CreateNewInstance(DbContextOptionsBuilder<TContext> optionsBuilder);
 
     private TContext CreateDbContext(string basePath, string environmentName)
     {
-        IConfigurationRoot config = new ConfigurationBuilder()
+        Configuration = new ConfigurationBuilder()
             .SetBasePath(basePath)
             .AddJsonFile("appsettings.json")
             .AddJsonFile($"appsettings.{environmentName}.json", true)
             .AddEnvironmentVariables()
             .Build();
 
-        string connectionString = config.GetConnectionString(ConnectionStringName);
-
-        return string.IsNullOrWhiteSpace(connectionString)
-            ? throw new InvalidOperationException($"Could not find a connection string named '{ConnectionStringName}'.")
-            : CreateNewInstance(
-            new DbContextOptionsBuilder<TContext>(),
-            connectionString);
+        return CreateNewInstance(new DbContextOptionsBuilder<TContext>());
     }
 }
