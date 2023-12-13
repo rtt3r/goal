63d5fe2ea00b5ba85a91d018e47b5a9fd509600a
Anderson Ritter de Souza <anderdsouza@gmail.com>
2023-12-13 02:23:20 -0300

    ajustes exceptions

--- src/Seedwork/Infra.Crosscutting/Notifications/Notification.cs
+++ src/Seedwork/Infra.Crosscutting/Notifications/Notification.cs
@@ -6,49 +6,49 @@ public record Notification(NotificationType Type, string Code, string Message, s
 {
     public static Notification Information(string code, string message)
     {
-        ArgumentException.ThrowIfNullOrWhiteSpace(code, nameof(code));
-        ArgumentException.ThrowIfNullOrWhiteSpace(message, nameof(message));
+        ArgumentException.ThrowIfNullOrWhiteSpace(code);
+        ArgumentException.ThrowIfNullOrWhiteSpace(message);
 
         return new(NotificationType.Information, code, message, null);
     }
 
     public static Notification InternalError(string code, string message)
     {
-        ArgumentException.ThrowIfNullOrWhiteSpace(code, nameof(code));
-        ArgumentException.ThrowIfNullOrWhiteSpace(message, nameof(message));
+        ArgumentException.ThrowIfNullOrWhiteSpace(code);
+        ArgumentException.ThrowIfNullOrWhiteSpace(message);
 
         return new(NotificationType.InternalError, code, message, null);
     }
 
     public static Notification ExternalError(string code, string message)
     {
-        ArgumentException.ThrowIfNullOrWhiteSpace(code, nameof(code));
-        ArgumentException.ThrowIfNullOrWhiteSpace(message, nameof(message));
+        ArgumentException.ThrowIfNullOrWhiteSpace(code);
+        ArgumentException.ThrowIfNullOrWhiteSpace(message);
 
         return new(NotificationType.ExternalError, code, message, null);
     }
 
     public static Notification InputValidation(string code, string message, string paramName)
     {
-        ArgumentException.ThrowIfNullOrWhiteSpace(code, nameof(code));
-        ArgumentException.ThrowIfNullOrWhiteSpace(message, nameof(message));
-        ArgumentException.ThrowIfNullOrWhiteSpace(paramName, nameof(paramName));
+        ArgumentException.ThrowIfNullOrWhiteSpace(code);
+        ArgumentException.ThrowIfNullOrWhiteSpace(message);
+        ArgumentException.ThrowIfNullOrWhiteSpace(paramName);
 
         return new(NotificationType.InputValidation, code, message, paramName);
     }
 
     public static Notification DomainViolation(string code, string message)
     {
-        ArgumentException.ThrowIfNullOrWhiteSpace(code, nameof(code));
-        ArgumentException.ThrowIfNullOrWhiteSpace(message, nameof(message));
+        ArgumentException.ThrowIfNullOrWhiteSpace(code);
+        ArgumentException.ThrowIfNullOrWhiteSpace(message);
 
         return new(NotificationType.DomainViolation, code, message, null);
     }
 
     public static Notification ResourceNotFound(string code, string message)
     {
-        ArgumentException.ThrowIfNullOrWhiteSpace(code, nameof(code));
-        ArgumentException.ThrowIfNullOrWhiteSpace(message, nameof(message));
+        ArgumentException.ThrowIfNullOrWhiteSpace(code);
+        ArgumentException.ThrowIfNullOrWhiteSpace(message);
 
         return new(NotificationType.ResourceNotFound, code, message, null);
     }
