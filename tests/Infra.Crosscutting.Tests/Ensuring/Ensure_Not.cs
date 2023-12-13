f65d1a439a215c75f585fc17a05e18cd7264bff2
Anderson Ritter de Souza <anderdsouza@gmail.com>
2023-12-13 00:20:30 -0300

    Ajustes testes ensure

--- tests/Infra.Crosscutting.Tests/Ensuring/Ensure_Not.cs
+++ tests/Infra.Crosscutting.Tests/Ensuring/Ensure_Not.cs
@@ -28,6 +28,20 @@ public class Ensure_Not
     }
 
     [Fact]
+    public void EnsureInvalidOperationExceptionGivenTrue()
+    {
+        Action act = () => Ensure.Not<InvalidOperationException>(true);
+        act.Should().Throw<InvalidOperationException>().And.Message.Should().Be("The expected condition was not exceeded");
+    }
+
+    [Fact]
+    public void EnsureInvalidOperationExceptionGivenFalse()
+    {
+        Action act = () => Ensure.Not<InvalidOperationException>(false);
+        act.Should().NotThrow<InvalidOperationException>();
+    }
+
+    [Fact]
     public void EnsureGivenFalseAndNotEmptyMessage()
     {
         Action act = () => Ensure.Not(false, "Test");
@@ -46,5 +60,47 @@ public class Ensure_Not
     {
         Action act = () => Ensure.Not(() => false);
         act.Should().NotThrow<Exception>();
+    }
+
+    [Fact]
+    public void ThrowExceptionGivenTruePredicateWithMessage()
+    {
+        Action act = () => Ensure.Not(() => true, "Test message");
+        act.Should().Throw<Exception>().And.Message.Should().Be("Test message");
+    }
+
+    [Fact]
+    public void EnsureGivenFalsePredicateWithMessage()
+    {
+        Action act = () => Ensure.Not(() => false, "Test message");
+        act.Should().NotThrow<Exception>();
+    }
+
+    [Fact]
+    public void ThrowInvalidOperationExceptionGivenTruePredicate()
+    {
+        Action act = () => Ensure.Not<InvalidOperationException>(() => true);
+        act.Should().Throw<InvalidOperationException>().And.Message.Should().Be("The expected condition was not exceeded");
+    }
+
+    [Fact]
+    public void EnsureInvalidOperationExceptionGivenFalsePredicate()
+    {
+        Action act = () => Ensure.Not<InvalidOperationException>(() => false);
+        act.Should().NotThrow<InvalidOperationException>();
+    }
+
+    [Fact]
+    public void ThrowInvalidOperationExceptionGivenTruePredicateWithMessage()
+    {
+        Action act = () => Ensure.Not<InvalidOperationException>(() => true, "Test message");
+        act.Should().Throw<InvalidOperationException>().And.Message.Should().Be("Test message");
+    }
+
+    [Fact]
+    public void EnsureInvalidOperationExceptionGivenFalsePredicateWithMessage()
+    {
+        Action act = () => Ensure.Not<InvalidOperationException>(() => false, "Test message");
+        act.Should().NotThrow<InvalidOperationException>();
     }
 }
