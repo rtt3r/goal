41d66a337e44f8f167192402a93d0ee22a1ad553
Anderson Ritter de Souza <anderdsouza@gmail.com>
2023-12-14 00:32:20 -0300

    fix command handlers

--- src/Seedwork/Application/Commands/ICommandHandler`.cs
+++ src/Seedwork/Application/Commands/ICommandHandler`.cs
@@ -3,7 +3,7 @@ using MediatR;
 namespace Goal.Seedwork.Application.Commands;
 
 public interface ICommandHandler<in TCommand, TResult> : IRequestHandler<TCommand, TResult>
-    where TCommand : class, ICommand<TResult>
-    where TResult : class, ICommandResult
+    where TCommand : ICommand<TResult>
+    where TResult : ICommandResult
 {
 }
