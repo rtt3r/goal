a00b012056fb10b78a8510c808ccd0cc22ed2d92
Anderson Ritter de Souza <anderdsouza@gmail.com>
2024-01-18 15:26:25 -0300

    improve some code

--- src/Seedwork/Infra.Crosscutting/Extensions/EnumerableExtensions.cs
+++ src/Seedwork/Infra.Crosscutting/Extensions/EnumerableExtensions.cs
@@ -8,30 +8,54 @@ namespace Goal.Seedwork.Infra.Crosscutting.Extensions;
 
 public static class EnumerableExtensions
 {
-    public static IEnumerable ForEach(this IEnumerable source, Action<object> action)
+    public static IEnumerable ForEach(this IEnumerable source, Action<object> body)
     {
         ArgumentNullException.ThrowIfNull(source);
-        ArgumentNullException.ThrowIfNull(action);
+        ArgumentNullException.ThrowIfNull(body);
+
+        List<Exception>? exceptions = null;
 
         foreach (object item in source)
         {
-            action(item);
+            try
+            {
+                body(item);
+            }
+            catch (Exception exc)
+            {
+                exceptions ??= [];
+                exceptions.Add(exc);
+            }
         }
 
-        return source;
+        return exceptions != null
+            ? throw new AggregateException(exceptions)
+            : source;
     }
 
-    public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, Action<T> action)
+    public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, Action<T> body)
     {
         ArgumentNullException.ThrowIfNull(source);
-        ArgumentNullException.ThrowIfNull(action);
+        ArgumentNullException.ThrowIfNull(body);
 
-        foreach (T element in source)
+        List<Exception>? exceptions = null;
+
+        foreach (T item in source)
         {
-            action(element);
+            try
+            {
+                body(item);
+            }
+            catch (Exception exc)
+            {
+                exceptions ??= [];
+                exceptions.Add(exc);
+            }
         }
 
-        return source;
+        return exceptions != null
+            ? throw new AggregateException(exceptions)
+            : source;
     }
 
     public static IPagedList<TResult> SelectPaged<TSource, TResult>(this IPagedList<TSource> source, Func<TSource, TResult> selector)
@@ -39,8 +63,4 @@ public static class EnumerableExtensions
         IEnumerable<TResult> items = source.Select(selector);
         return new PagedList<TResult>(items, source.TotalCount);
     }
-
-    public static string Join(this IEnumerable<string> values, string separator) => string.Join(separator, values);
-
-    public static string Join<T>(this IEnumerable<T> values, string separator) => string.Join(separator, values);
 }
--- src/Seedwork/Infra.Crosscutting/Specifications/AndSpecification.cs
+++ src/Seedwork/Infra.Crosscutting/Specifications/AndSpecification.cs
@@ -6,7 +6,6 @@ namespace Goal.Seedwork.Infra.Crosscutting.Specifications;
 public sealed class AndSpecification<TEntity>(ISpecification<TEntity> leftSideSpecification, ISpecification<TEntity> rightSideSpecification) : CompositeSpecification<TEntity>(leftSideSpecification, rightSideSpecification)
     where TEntity : class
 {
-
     public override Expression<Func<TEntity, bool>> SatisfiedBy()
     {
         Expression<Func<TEntity, bool>> left = LeftSideSpecification.SatisfiedBy();
