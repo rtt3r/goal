abfe22a40ade31332208dfcf4fd410a601b1af74
Anderson Ritter de Souza <AndersonSO@abcbrasil.com.br>
2024-06-01 00:03:04 -0300

    refact: update query repository

--- src/Abstractions/Infra.Data.Query/IQueryRepository`.cs
+++ src/Abstractions/Infra.Data.Query/IQueryRepository`.cs
@@ -8,7 +8,7 @@ namespace Goal.Infra.Data.Query;
 public interface IQueryRepository<TEntity, TKey> : IQueryRepository
     where TEntity : class
 {
-    Task<TEntity> LoadAsync(TKey id, CancellationToken cancellationToken = new CancellationToken());
+    Task<TEntity?> LoadAsync(TKey id, CancellationToken cancellationToken = new CancellationToken());
     Task<ICollection<TEntity>> QueryAsync(CancellationToken cancellationToken = new CancellationToken());
     Task<IPagedList<TEntity>> QueryAsync(IPageSearch pageSearch, CancellationToken cancellationToken = new CancellationToken());
     Task StoreAsync(TKey id, TEntity entity, CancellationToken cancellationToken = new CancellationToken());
--- src/Abstractions/Infra.Data.Query/QueryRepository`.cs
+++ src/Abstractions/Infra.Data.Query/QueryRepository`.cs
@@ -8,7 +8,7 @@ namespace Goal.Infra.Data.Query;
 public abstract class QueryRepository<TEntity, TKey> : QueryRepository, IQueryRepository<TEntity, TKey>
     where TEntity : class
 {
-    public abstract Task<TEntity> LoadAsync(TKey id, CancellationToken cancellationToken = new CancellationToken());
+    public abstract Task<TEntity?> LoadAsync(TKey id, CancellationToken cancellationToken = new CancellationToken());
     public abstract Task<ICollection<TEntity>> QueryAsync(CancellationToken cancellationToken = new CancellationToken());
     public abstract Task<IPagedList<TEntity>> QueryAsync(IPageSearch pageSearch, CancellationToken cancellationToken = new CancellationToken());
     public abstract Task StoreAsync(TKey id, TEntity entity, CancellationToken cancellationToken = new CancellationToken());
