851b3217230cd69779e2831c6fe4307784fb23c5
Anderson Ritter de Souza <anderdsouza@gmail.com>
2024-10-16 17:49:08 -0300

    fix command handler

--- src/Abstractions/Application/Commands/ICommandHandler``.cs
+++ src/Abstractions/Application/Commands/ICommandHandler``.cs
@@ -2,7 +2,7 @@ using MediatR;
 
 namespace Goal.Application.Commands;
 
-public interface ICommandHandler<in TCommand, TResult> : ICommandHandler<TCommand>, IRequestHandler<TCommand, TResult>
+public interface ICommandHandler<in TCommand, TResult> : IRequestHandler<TCommand, TResult>
     where TCommand : ICommand<TResult>
 {
 }
