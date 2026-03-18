d6fe87ce1f8734d4102c2601cb486f79d1f2b3a7
Anderson Ritter de Souza <anderdsouza@gmail.com>
2026-03-18 17:50:17 -0300

    Bump version to 10.0.6 for all project files and update ConduitRCommandSender access modifier to public

--- src/Abstractions/Application/Commands/ConduitRCommandSender.cs
+++ src/Abstractions/Application/Commands/ConduitRCommandSender.cs
@@ -2,7 +2,7 @@ using ConduitR.Abstractions;
 
 namespace Goal.Application.Commands;
 
-internal sealed class ConduitRCommandSender(IMediator mediator) : ICommandSender
+public sealed class ConduitRCommandSender(IMediator mediator) : ICommandSender
 {
     public ValueTask<TResponse> Send<TResponse>(ICommand<TResponse> command, CancellationToken cancellationToken = default)
         => mediator.Send(command, cancellationToken);
